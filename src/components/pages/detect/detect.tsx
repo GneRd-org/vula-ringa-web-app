11e086fa28bd453c66d0e79e75e2ff5de4ab6d69
refilwe-m <refilwe.dev@gmail.com>
2024-06-30 08:18:35 +0200

    Detect

--- src/components/pages/detect/detect.tsx
+++ src/components/pages/detect/detect.tsx
@@ -1,21 +1,23 @@
+import { VoiceRecorder } from "../../common";
+
 export const Detect = () => {
   return (
     <section className="h-full w-full flex flex-col gap-6">
-      <section id="about-us" className="w-full flex items-center">
+      <section className="w-full flex items-center">
         <section className="w-full flex flex-col gap-4">
           <section className="bg-lightPurple h-58 px-3 py-5 rounded-bl-3xl">
             <h1 className="text-5xl font-extrabold text-center text-white">
               Detect
             </h1>
             <section className="flex items-center justify-around w-full">
-              <p className="py-10 text-xs text-black font-bold w-2/3">
-                Your friend in a foreign land, which helps you to communicate
-                with people who speak different languages.
+              <p className="py-10 text-xs text-center text-black font-bold w-full">
+                Let's help you detect the language around you.
               </p>
             </section>
           </section>
         </section>
       </section>
+      <VoiceRecorder detect transcribe={false} />
     </section>
   );
 };
